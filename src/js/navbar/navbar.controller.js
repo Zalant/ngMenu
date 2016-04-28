0a1a4e3db6ecf94cc74a8d39ff3259cc887aa413
Zalant <iamzalant@gmail.com>
2016-04-28 23:47:29 +0300

    1.2.3

--- src/js/navbar/navbar.controller.js
+++ src/js/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 (function (angular) {
   angular.module('navbar')
-    .controller('menuCtrl', ['$scope','$state', function ($scope,$state) {
+    .controller('menuCtrl', ['$scope', '$state', function ($scope, $state) {
 
 
       //Здесь задаются пункты меню. Состояния к ним прописываются в navbar.module.js
@@ -16,7 +16,7 @@
         {name: 'test-3', url: '#', state: 'test-3'},
         {
           name: 'test-4', url: "#",
-          location: ['test-5']
+          location: ['test-5', 'test-8']
         },
         {name: 'test-7', url: '#', state: 'test-7'},
         {
@@ -26,7 +26,8 @@
         {
           name: 'test-6', url: '#',
           location: ['test-7']
-        }
+        },
+        {name: 'test-8', url: '#', state: 'test-8'}
 
       ];
 
@@ -58,31 +59,47 @@
           if (place.pop) {
             for (var j = 0; j < place.length; j++) {
               var dependence = place[j];
+
+              if (dependence === obj.name) {
+                console.log('Warning! Пункт "' + dependence + '" содержит в подпунктах сам себя.');
+                return;
+              }
+              //проверка на наличие данного подпункта в общем списке
+              var flag = false;
+              for (var i = 0; i < menuItems.length; i++) {
+                if (dependence === menuItems[i].name) {
+                  flag = true;
+                }
+              }
+              if (!flag && !dependence.name) {
+                console.log('Warning! Пункта "' + dependence + '" не существует в общем списке.');
+              }
+
               for (var k = 0; k < menuItems.length; k++) {
                 if (dependence === menuItems[k].name) {
                   place[j] = menuItems[k];
                   //флаг для того, чтобы не выводить в основном меню пункты, которые являются подпунктами
                   menuItems[k].submenu = true;
+
                   checkElement(place[j], menuItems);
                 }
               }
             }
           }
-
         }
       }
 
-      //Функция добавления нового пункта на первый уровень меню
+      //Функция добавления нового пункта меню
 
       function add(obj) {
-        if (!obj.location) {
+        if (obj.location) {
+        } else {
           var name = obj.name;
           for (var i = 0; i < menuItems.length; i++) {
-            var currentName = (menuItems[i].name.pop) ? menuItems[i].name[0] : menuItems[i].name;
+            var currentName = menuItems[i].name;
             if (currentName === name) {
               console.log('Warning! Меню дублируется "' + name + '".');
               return;
-
             }
           }
           menuItems.push(obj);
@@ -92,31 +109,7 @@
         return;
       };
 
-
       $scope.menuList = menuItems;
-      
-
-      // function checkState() {
-      //   console.log($state.current.name);
-      // }
-      // document.onmousedown = checkState;
-
-      // function checkClick(evt)
-      // {
-      //   evt = (evt) ? evt : event;
-      //   if(evt.shiftKey)
-      //   {
-      //     console.log(
-      //       "Нажата клавиша Shift");
-      //   }
-      //   else
-      //   {
-      //     console.log(
-      //       "Клавиша Shift не нажата");
-      //   }
-      // }
-      // document.onmousedown = checkClick;
-
 
     }]);
 })(window.angular);
--- src/js/navbar/navbar.module.js
+++ src/js/navbar/navbar.module.js
@@ -1,28 +1,32 @@
 'use strict';
 (function () {
-    angular.module('navbar', ['ui.router', 'ngStorage'])
-            .config(['$stateProvider','$urlRouterProvider',
-                    function ($stateProvider, $urlRouterProvider) {
+  angular.module('navbar', ['ui.router', 'ngStorage'])
+    .config(['$stateProvider', '$urlRouterProvider',
+      function ($stateProvider, $urlRouterProvider) {
 //      $urlRouterProvider.otherwise('/first');
-                        $stateProvider
-                                .state('main', {
-                                    url: '/main',
-                                    templateUrl: 'states/mainpage.html'
-                                })
-                                .state('test-1', {
-                                    url: '/test1',
-                                    templateUrl: 'states/test-1.html'
-                                })
-                                .state('test-3', {
-                                    url: '/test3',
-                                    templateUrl: 'states/test-3.html'
-                                })
-                                .state('test-7', {
-                                    url: '/test7',
-                                    templateUrl: 'states/test-7.html'
-                                })
-                              ;
+        $stateProvider
+          .state('main', {
+            url: '/main',
+            templateUrl: 'states/mainpage.html'
+          })
+          .state('test-1', {
+            url: '/test1',
+            templateUrl: 'states/test-1.html'
+          })
+          .state('test-3', {
+            url: '/test3',
+            templateUrl: 'states/test-3.html'
+          })
+          .state('test-7', {
+            url: '/test7',
+            templateUrl: 'states/test-7.html'
+          })
+          .state('test-8', {
+            url: '/test8',
+            templateUrl: 'states/test-8.html'
+          })
+        ;
 
-                    }
-            ]);
+      }
+    ]);
 })();
